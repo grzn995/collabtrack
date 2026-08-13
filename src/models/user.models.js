27838bf62cb969b04d4416bc981dc1347f8d491f
David Gvenetadze <davidgvenetadze3@gmail.com>
2026-08-14 00:10:35 +0200

    fixed bug in previous commit of comparing password against hash

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -73,7 +73,7 @@ userSchema.pre("save", async function(next){
 })
 
 userSchema.methods.isPasswordCorrect = async function(password){
-  reeturn await bcrypt.compare(password, this.password);
+  return await bcrypt.compare(password, this.password);
   
 }
 
