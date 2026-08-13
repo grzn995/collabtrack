15a4a09d08835314eb4cf52ee21fd84a31232b39
David Gvenetadze <davidgvenetadze3@gmail.com>
2026-08-13 23:52:15 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from "express"
+import cors from "cors"
+const app = express()
+
+
+//basic configuration
+app.use(express.json({limit : "16kb"}))
+app.use(express.urlencoded({extended : true, limit : "16kb"}))
+app.use(express.static("public"))
+
+//cors configuration
+app.use(cors({
+  origin : process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+  credentials : true,
+  methods : ["GET","POST","PATCH","DELETE","OPTIONS"],
+  allowedHeaders : ["Authorization", "Content-Type"]
+}))
+//import the routes
+
+import {router} from "./routes/healthcheck.routes.js"
+
+app.use("/api/v1/healthcheck", router)
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+export default app;
