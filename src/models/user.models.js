34a383fd643a40377322ee0926f09e84d4149ad0
David Gvenetadze <davidgvenetadze3@gmail.com>
2026-08-14 01:25:51 +0200

    implemented refresh and access token

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,7 +1,6 @@
 import { Schema } from "mongoose";
 import mongoos from "mongoose";
-
-
+import jwt from "jsonwebtoken"
 const userSchema = new Schema({
 
   avatar : {
@@ -49,8 +48,8 @@ const userSchema = new Schema({
   },
   forgotPasswordExpiry : {
     type : Date
-  },
-  emailVerificationToken : {
+    },
+    emailVerificationToken : {
     type: String
   },
   emailVerficationExpiry : {
@@ -76,6 +75,28 @@ userSchema.methods.isPasswordCorrect = async function(password){
   return await bcrypt.compare(password, this.password);
   
 }
+
+userSchema.methods.generateAccessToken = function(){
+  return jwt.sign(
+    {
+      _id : this._id,
+      email: this.email,
+      username : this.username
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {expiresIn : process.env.ACCESS_TOKEN_EXPIRY}
+  )
+}
+userSchema.methods.generateRefreshToken = function(){
+  return jwt.sign(
+    {
+      _id : this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {expiresIn : process.env.REFRESH_TOKEN_EXPIRY}
+  )
+}
+
 
 export const User = mongoose.model("User", userSchema)
 
