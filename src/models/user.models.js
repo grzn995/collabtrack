3583b53c28e840fd96857b70419b401c73339e55
David Gvenetadze <davidgvenetadze3@gmail.com>
2026-08-14 00:09:44 +0200

    added method to user schema for comparing password against hash

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -72,6 +72,10 @@ userSchema.pre("save", async function(next){
 
 })
 
+userSchema.methods.isPasswordCorrect = async function(password){
+  reeturn await bcrypt.compare(password, this.password);
+  
+}
 
 export const User = mongoose.model("User", userSchema)
 
