d6b6f11927d6635f650f515acf366adec0a4b455
David Gvenetadze <davidgvenetadze3@gmail.com>
2026-08-14 21:36:07 +0200

    implemented validator middleware and the validator function in routers

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/auth.controller.js";
+import { validate } from "../middlewares/validator.middleware.js";
+import {userRegisterValidator} from "../validators/index.js"
+
+
 const authRouter = Router()
 
-authRouter.route("/register").post(registerUser)
+authRouter.route("/register").post(userRegisterValidator() , validate, registerUser)
 
 export {authRouter}
