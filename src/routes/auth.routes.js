604eeeef17648577f66bec1631d51a2ede1b74e0
David Gvenetadze <davidgvenetadze3@gmail.com>
2026-08-15 18:09:38 +0200

    implemented all routes as listed in the PRD

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,27 @@
 import { Router } from "express";
-import { login, logout, registerUser } from "../controllers/auth.controller.js";
+import { changeCurrentPassword, forgotPasswordRequest, getCurrentUser, login, logout, refreshAccessToken, registerUser, resendEmailVerification, resetForgotPassword, verifyEmail } from "../controllers/auth.controller.js";
 import { validate } from "../middlewares/validator.middleware.js";
-import {userLoginValidator, userRegisterValidator} from "../validators/index.js"
+import {userChangeCurrentPasswordValidator, userForgotPasswordValidator, userLoginValidator, userRegisterValidator, userResetForgotPasswordValidator} from "../validators/index.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const authRouter = Router()
 
 authRouter.route("/register").post(userRegisterValidator() , validate, registerUser)
 authRouter.route("/login").post(userLoginValidator(), validate, login)
+authRouter.route("verify-email/:verificationToken").get(verifyEmail)
+authRouter.route("/refresh-token").post(refreshAccessToken)
+authRouter.route("/forgot-password").post(userForgotPasswordValidator(),validate,forgotPasswordRequest)
+authRouter.route("/reset-password:resetToken").post(userResetForgotPasswordValidator(),validate,resetForgotPassword)
+
+
+
+
 authRouter.route("/logout").post(verifyJWT, logout)
+authRouter.route("/current-user").post(verifyJWT, getCurrentUser)
+authRouter.route("/change-password").post(verifyJWT, userChangeCurrentPasswordValidator(),validate,changeCurrentPassword)
+authRouter.route("/resend-email-verification").post(verifyJWT, resendEmailVerification)
+
+
+
 
 export {authRouter}
