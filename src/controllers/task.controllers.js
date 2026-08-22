ee316e8577052d10dec06f81c194a15726fcbe99
David Gvenetadze <davidgvenetadze3@gmail.com>
2026-08-22 19:50:24 +0200

    implement createTask and getTask

---
+++ src/controllers/task.controllers.js
@@ -0,0 +1,67 @@
+import {Task} from "../models/task.models.js"
+import {Project} from "../models/project.models.js"
+import {Subtask} from "../models/subtask.models.js"
+import { ApiError } from "../utils/api-error.js"
+import { ApiResponse } from "../utils/api-response.js"
+import { asyncHandler } from "../utils/asynchandler.js"
+import mongoose from "mongoose"
+import { AvailableUserRole,UserRolesEnum } from "../utils/constants.js"
+
+
+const getTasks = asyncHandler(async(req,res)=>{
+  const {projectId} = req.params
+  const project = await Project.findById(projectId)
+  if(!project){
+    throw new ApiError(404,"Project not found")
+  }
+
+  const tasks = await Task.find({
+    project : new mongoose.Types.ObjectId(projectId)
+  }).populate("assignedTo", "avatar username fullName")
+
+  return res.status(200).json(new ApiResponse(200,tasks,"Tasks fetched successfully"))
+
+})
+const createTask= asyncHandler(async(req,res)=>{
+  const {title,description,assignedTo,status} = req.body
+  const {projectId} = req.params
+  const project = await Project.findById(projectId)
+  if(!project){
+    throw new ApiError(404,"Project not found")
+  }
+
+  const files = req.files || []
+
+  const attachments = files.map((file) => {
+    return {
+      url : `${process.env.SERVER_URL}/images/${file.originalName}`,
+      mimetype : file.mimetype,
+      size : file.size
+    }
+  })
+
+
+  const task = await Task.create({
+    title,
+    description,
+    project : new mongoose.Types.ObjectId(projectId),
+    assignedTo : assignedTo ? new mongoose.Types.ObjectId(assignedTo) : undefined,
+    status,
+    assignedBy: new mongoose.Types.ObjectId(req.user._id),
+    attachments
+  })
+
+  return res.status(201).json(new ApiResponse(201,task,"Task created successfully"))
+
+
+})
+const getTaskById= asyncHandler(async(req,res)=>{})
+const deleteTask= asyncHandler(async(req,res)=>{})
+const updateTask= asyncHandler(async(req,res)=>{})
+const createSubtask= asyncHandler(async(req,res)=>{})
+const updateSubtask= asyncHandler(async(req,res)=>{})
+const deleteSubtask= asyncHandler(async(req,res)=>{})
+
+
+
+export {getTasks,createTask,getTaskById,deleteTask,deleteSubtask,updateTask,createSubtask,updateSubtask}
